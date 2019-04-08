f2ffe88a553986704a26c01de86ec18db4011d65
Irina Penzina <irinapenzina@mail.ru>
2019-04-08 16:38:58 +0800

    validations

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,14 @@
 class Hospital < ApplicationRecord
   has_many :reviews
   has_many :appointments
+
+  validates :image, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :open_at, presence: true
+  validates :close_at, presence: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
 end
+
