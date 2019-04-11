0f155be3bf050213da72217a284c8f8a0dab48ab
Tracy Lee <tracylee1216@gmail.com>
2019-04-11 15:40:03 +0800

    added tags with filter

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,25 +1,22 @@
 class HospitalsController < ApplicationController
-
   # skip_before_action :authenticate_user!, only: [:index, :show]
-
   before_action :set_hospital, only: :show
 
-
   def index
-    @hospitals = Hospital.all
+    if params[:tag].present?
+      @hospitals = Hospital.tagged_with(params[:tag])
+    else
+      @hospitals = Hospital.all
+    end
+    @insurers = ["Allianz", "AXA PPP", "AXA ICBC", "AXA Tianping", "BUPA", "CIGNA", "Discovery Health", "Generali", "MSH", "NOW Health"]
+    @provider_types = ["private", "public"]
+    @languages = ["chinese", "english", "cantonese", "french", "spanish", "russian"]
+    @specialties = ["family medicine", "pediatrics", "cardiology", "dermatology"]
   end
 
   def show
     # displaying comments
     @comment = Comment.new
-  end
-
-  def tagged
-    if params[:tag].present?
-      @hospitals = Hospital.tagged_with(params:[:tag])
-    else
-      @hospitals = Hospital.all
-    end
   end
 
   private
@@ -28,8 +25,5 @@ class HospitalsController < ApplicationController
     @hospital = Hospital.find(params[:id])
   end
 
-  def comment_params
-    params.require(:comment).permit(:title, :comment)
-  end
 
 end
