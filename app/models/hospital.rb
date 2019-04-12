6fa03963351f205f8cf654299270ea2e116f350f
Nini Zhu <pn.zhu76@gmail.com>
2019-04-12 11:03:47 +0800

    generated Hangzhou hospitals seeds

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -5,11 +5,10 @@ class Hospital < ApplicationRecord
   validates :image, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true, uniqueness: true
-  validates :price, presence: true
   validates :open_at, presence: true
   validates :close_at, presence: true
-  validates :longitude, presence: true, uniqueness: true
-  validates :latitude, presence: true, uniqueness: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
   validates :tel, presence: true, uniqueness: true
 
   acts_as_commentable
