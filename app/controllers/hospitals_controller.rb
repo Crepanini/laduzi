9bfaf55ccf9e80e7248854b4a2c731611163b92c
Irina Penzina <irinapenzina@mail.ru>
2019-04-19 16:19:30 +0800

    button change

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -8,19 +8,51 @@ class HospitalsController < ApplicationController
     @languages = ["Cn", "Eng", "Canto", "Fr", "Span", "Ru", "Jp", "Kr", "Ger", "Farsi", "Dutch", "Filipino"]
     @specialties = ["Acupuncture", "Cardiology", "Dental", "Dental Care", "Ear, Nose & Throat", "Endocrinology", "Gastroenterology", "General Medicine", "General Surgery", "Gynecology", "Haematology", "Infectious Disease", "Internal Medicine", "Kidney Disease", "Liver Disease", "Neurology", "Nuclear Medicine", "OB & GYN", "Ophthalmology", "Organ Transplants", "Orthopaedics", "Osteology", "Pediatrics", "Physical Checkup", "Pneumology", "Respiratory", "Rheumatology", "Sarcoidosis", "Sport Medicine", "Traditional Chinese Medicine", "TCM physiotherapy", "Tumors", "Urology"]
 
-
-    @hospitals = Hospital.all
-
-    if params.key?("tag") and !params[:tag].empty?
+    # if params[:city].blank?
+    #   if params[:tag].blank?
+    #     p "no city, no tag"
+    #     @hospitals = Hospital.all
+    #   else
+    #     p "no city, with tags"
+    #     @hospitals = Hospital.all.tagged_with(params[:tag])
+    #   end
+    # elsif params[:tag].blank?
+    #     p "with city, no tags"
+    #     location_filter
+    #   else
+    #     p "with city, with tags"
+    #     location_filter
+    #     @hospitals = @hospitals.tagged_with(params[:tag])
+    #   end
+    # end
+    if !params[:city].blank? && !params[:tag][0].blank?
+      p "with city, with tags"
+      p params[:tag][0].blank?
+      p params[:tag][0].empty?
+      p params[:tag][0].nil?
+      p
       location_filter
       @hospitals = @hospitals.tagged_with(params[:tag])
-    else
+    end
+
+    if params[:city].blank? && params[:tag][0].blank?
+      p "no city, no tag"
+      @hospitals = Hospital.all
+    end
+
+    if params[:city].blank? && !params[:tag][0].blank?
+      p "no city, with tags"
+      @hospitals = Hospital.all.tagged_with(params[:tag])
+    end
+
+    if !params[:city].blank? && params[:tag][0].blank?
+      p "with city, no tags"
       location_filter
     end
 
     # search bar
     if params[:query].present?
-      return @hospitals = Hospital.where("name @@ ?", "%#{params[:query]}%")
+      @hospitals = Hospital.where("name @@ ?", "%#{params[:query]}%")
     end
 
     set_markers
